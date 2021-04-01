6d03e93b2f3c6d81bde462e8f7510fd8fdbf9312
Attila Barna <attila.barna.wr@gmail.com>
2021-04-01 16:02:10 +0200

    Context menu for managing menu items.

---
+++ src/components/ui/context-menu/TreeContextMenu.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import useContextMenu from "./ContextMenu";
+import TreeContextMenuItem from "./TreeContextMenuItem";
+
+const Menu = styled.div`
+  cursor: pointer;
+  position: absolute;
+  border: 1px solid black;
+  background-color: white;
+  width: 200px;
+  height: 200px;
+  overflow: hidden;
+  z-index: 5000;
+`;
+
+/**
+ * Attila Barna
+ * @param {*} param0 
+ */
+const TreeContextMenu = ({ contextMenuRef, onItemClick,  items }) => {
+  const { leftPos, topPos, menu } = useContextMenu(contextMenuRef);
+
+  if (menu) {
+    return (
+      <Menu style={{  top: topPos, left: leftPos }}>
+        {
+            items.map( (e,ix) => (
+                 <TreeContextMenuItem key={ix} click={onItemClick} item={e} ></TreeContextMenuItem>
+            ))
+        }
+      </Menu>
+    );
+  }
+  return <></>;
+};
+
+export default TreeContextMenu;
